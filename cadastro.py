4a0044167a635961900e1c5824895ebc7f7dfabb
EduardoMartins038 <eduardo.b1.martins@gmail.com>
2024-02-24 22:07:58 -0300

    Update

--- cadastro.py
+++ cadastro.py
@@ -10,31 +10,27 @@ firebase_admin.initialize_app(cred)
 
 # Inicialize o Firestore
 db = firestore.client()
+def salvar_cliente(nomec, cpf, datanascimento, endereco, contato, nome):
+    # Crie um documento no banco de dados com o CPF como ID
+    doc_ref = db.collection('clientes').document(nome)
 
-# Defina os dados que você deseja adicionar ao documento
-dados_documento = {
-    u'Nome': u'Arthur',
-    u'Descrição': u'Lindo',
-    # Adicione outros campos conforme necessário
-}
+    # Adicione os dados do cliente ao documento
+    doc_ref.set({
+        'nomec': nomec,
+        'cpf': cpf,
+        'datanascimento': datanascimento,
+        'endereco': endereco,
+        'contato': contato,
+        'nome': nome
+    })
 
-# Adicione os dados ao Firestore criando uma coleção e um documento
-# A coleção pode ser criada automaticamente quando você adiciona um documento
-doc_ref = db.collection(u'suaColecao').document()
-
-# Insira os dados no documento recém-criado
-doc_ref.set(dados_documento)
-
-print("Coleção e documento criados com sucesso!")
-
-
-#Automação Cadastro
-print('Olá, Seja bem-vindo á IBEV! Para realizar o seu cadastro é necessário preencher os seguintes dados:')
+# Solicite os dados do cliente ao usuário
 nomec=input('Informe por favor seu nome completo:')
 cpf=input('Digite seu CPF:')
 datanascimento=input('Informe por favor sua data de nascimento:')
 endereco=input('Informe por favor seu endereço completo, Exemplo: *Av. Crisantino de Almeida, 590 - Vargem Grande II, Montes Claros - MG, 39403-452* ')
 contato=input('Qual o telefone para contato?')
 nome=input('Como gostaria de ser chamado?')
-confirm=input('Maravilha {}, Agora me confime os dados informados: Seu nome completo é: {}, Seu Cpf: {}, Sua data de nascimento: {}, Seu endereco: {}, Sua naturalidade: {}, Seu telefone para contato: {}'
-.format(nome, nomec, cpf, datanascimento, endereco, contato))
+
+# Salve os dados do cliente no banco de dados firebase
+salvar_cliente(nomec, cpf, datanascimento, endereco, contato, nome)
